0d4a2698675c60d1bace31bd26633b27aa24555f
Fran ʚĭɞ <2013talgia@gmail.com>
2026-03-17 20:07:56 -0300

    js

---
+++ main.js
@@ -0,0 +1,9 @@
+let numbers = [10, 30, 40, 60, 80]
+
+//acessando o primeiro elemento
+console.log(numbers[0]); //no caso, estamos acessando o número 10, já que sua posição é 0.
+//sempre do 0 ao número máximo decidido
+
+//acessando o último elemento
+console.log(numbers[4]);
+
